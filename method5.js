8cf4acb00ae1d2515214180f864194d4b376ce04
Nehya J <168397797+Nehyajeevam@users.noreply.github.com>
2025-07-11 21:21:45 +0530

    Add files via upload

---
+++ method5.js
@@ -0,0 +1,7 @@
+let number = 123; 
+if (number >= 100 && number <= 999) {
+    console.log("It is a 3-digit number.");
+} 
+else {
+    console.log("It is NOT a 3-digit number.");
+}
---
+++ method6.js
@@ -0,0 +1,12 @@
+let a = 35;
+let b = 23;
+let c = 66;
+if (a >= b && a >= c) {
+    console.log("The largest number is:"+a);
+} 
+else if (b >= a && b >= c) {
+    console.log("The largest number is:"+b);
+}
+ else {
+    console.log("The largest number is:"+c);
+}
